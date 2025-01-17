0dd0097a9112aff70e1264575c00628964f640d2
Rokalia <ksi_ts@mail.ru>
2025-01-17 18:21:40 +0300

    Была добавлена модель User

--- database.py
+++ database.py
@@ -5,6 +5,24 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 metadata = MetaData()
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    
+    def __init__(self, name, mail, pswrd) -> None:
+        self.username = name
+        self.email = mail
+        self.password = pswrd
+    
+    def __repr__(self) -> str:
+        return '<User %r>' % self.name
+ 
+def create_standart_tables():
+    metadata.create_all(bind=db.engine)
+
 def initialization(app):
     db.init_app(app)
 
@@ -19,7 +37,7 @@ def create_results_table(table_name, elements):
         columns.append(Column(column_name, column_type))
 
     table = Table(table_name, metadata, *columns, extend_existing=True)
-    metadata.create_all(bind=db.engine)  # Ñîçäàåì òàáëèöó â áàçå äàííûõ
+    metadata.create_all(bind=db.engine)
     return table
 
 def insert_parsing_results(table_name, results):
